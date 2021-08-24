738043f6db466799732f7c62025df654d99aa8a2
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-08-24 18:40:56 -0500

    beginning

--- SpinningDiscs/Program.cs
+++ SpinningDiscs/Program.cs
@@ -9,6 +9,8 @@ namespace SpinningDiscs
             // TODO: Declare and initialize a CD and a DVD object.
 
             // TODO: Call each CD and DVD method to verify that they work as expected.
+
+            //hi
         }
     }
 }
